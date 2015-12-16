2b9dbf85c7c2edbf9ead17f4801c65b53452420f
Dominic May <jack.thatch@gmail.com>
2015-12-16 14:15:50 +0800

    Load SourceCodePro for labels

--- renderer.py
+++ renderer.py
@@ -1,10 +1,11 @@
 from itertools import chain, count
 from matplotlib.cm import get_cmap
 
-from PIL import Image
+from PIL import Image, ImageFont
 from PIL.ImageDraw import ImageDraw
 
 CMAP = get_cmap('rainbow')
+SOURCE_CODE_PRO = ImageFont.truetype('SourceCodePro-Regular.otf', size=25)
 
 BASE = 50
 RATIO = 70 / 50
