6826bc32e227e0aa0330ae636c54ab3b7c32b411
Dominic May <me@mause.me>
2018-08-18 19:52:19 +0800

    Bind to all interfaces

--- api/server.py
+++ api/server.py
@@ -277,7 +277,11 @@ def schema_route():
 
 
 def main():
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(
+        debug=True,
+        port=int(os.environ.get('PORT', 5000)),
+        host='0.0.0.0'
+    )
 
 
 if __name__ == '__main__':
