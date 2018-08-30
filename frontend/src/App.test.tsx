46e7d22695416360878e01374d42b1cea4c16e32
Dominic May <me@mause.me>
2018-08-30 20:01:03 +1000

    Enable running frontend tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { StaticRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const el = <StaticRouter context={{}}><App /></StaticRouter>;
+  ReactDOM.render( el, div);
   ReactDOM.unmountComponentAtNode(div);
 });
