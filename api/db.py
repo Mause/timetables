b364b45443bd146f3ba0a6ee2a7f23301339c1e6
Dominic May <me@mause.me>
2018-08-31 15:05:37 +1000

    Restore location column

--- api/db.py
+++ api/db.py
@@ -36,7 +36,7 @@ ClassInstance = type(
     {
         'start': orm.Required(Arrow),
         'end': orm.Required(Arrow),
-        'location': str,
+        'location': orm.Optional(str),
         'class': orm.Required('Class'),
     }
 )
